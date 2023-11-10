19beeafa96401a684c78b46e9a4d437772b527c2
KavyaByreddy <kavyabyreddy68@gmail.com>
2023-11-10 17:19:37 +0530

    k

---
+++ SpringBoot-Backend/src/main/java/com/example/SpringBoot/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.SpringBoot.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
